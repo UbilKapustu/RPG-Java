a103adfa678d0b53b99e6e13a0fe8aab66db9b96
Dzmlv <40449@iitu.edu.kz>
2026-02-13 23:25:43 +0500

    update

--- src/game/Player.java
+++ src/game/Player.java
@@ -14,7 +14,26 @@ public class Player {
         this.charClass=charClass;
         this.inventory= new Inventory();
         switch(charClass){
-            
+            case "Батыр":
+                health=150;
+                attack=15;
+                defense=10;
+                stamina= 12;
+                mana=0;
+                break;
+            case "Шаман":
+                health = 75;
+                attack= 20;
+                defense = 5;
+                stamina=2;
+                mana=12;
+                break;
+            case "Сайгакшы":
+                health=100;
+                attack=17;
+                defense=8;
+                stamina=8;
+                mana=8;
         }
     
     }
--- src/game/Weapons.java
+++ src/game/Weapons.java
@@ -1,6 +1,9 @@
 package game;
 
 public class Weapons {
+    String name;
+    String type;
+    int mana_loss;
     int attack_add;
     int stamina_loss;
 }
